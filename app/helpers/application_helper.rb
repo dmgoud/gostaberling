e4c5f1ab0d65b562dbf3409dc1e188ecf9e83c93
David Goudreau <dmgoud@gmail.com>
2009-05-14 07:10:01 -0700

    First cut at new site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def currently_selected(selected)
+    selected == @controller.controller_name
+  end
+
 end
