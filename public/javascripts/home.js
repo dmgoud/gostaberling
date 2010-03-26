664e8e96db991dd152a0fa29be10de0233392735
David Goudreau <pair+david@pivotallabs.com>
2010-03-25 23:10:42 -0700

    Updating home page

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $("a#flyer").addClass("thickbox").attr("href", "/images/flyer-tenement-sm.png?height=600&amp;width=400&amp;inlineId=popup");
+  $("a#flyer").addClass("thickbox").attr("href", "/images/gb_couch1_kimos_022510a_big.jpg?height=600&amp;width=400&amp;inlineId=popup");
   
   tb_init('a.thickbox');
   
