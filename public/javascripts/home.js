96f8f47ca0f51cfc4e4a4d7415f15f7aa92a94ad
David Goudreau <pair+david@pivotallabs.com>
2010-04-05 13:47:16 -0700

    Removing js files, adding them to index files

--- public/javascripts/home.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-  $("a#flyer").addClass("thickbox").attr("href", "/images/gb_couch1_kimos_022510a_big.jpg?height=600&amp;width=400&amp;inlineId=popup");
-  
-  tb_init('a.thickbox');
-  
-});
-
-
