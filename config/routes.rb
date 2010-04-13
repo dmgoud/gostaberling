caff42b63a0ae2511e4bbbc605b24fbb0bafba0d
David Goudreau <pair+david@pivotallabs.com>
2010-04-12 20:26:19 -0700

    lots of fixes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :pics, :only => [:index], :collection => {:gallery => :get}
 
+  map.signingup 'signingup', :controller => :info, :action => 'signingup'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
