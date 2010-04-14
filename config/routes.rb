dc118d51f99dfa1ef5c1faccc3e8a38dfb0934ed
David Goudreau <pair+david@pivotallabs.com>
2010-04-14 16:52:47 -0700

    setting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 ActionController::Routing::Routes.draw do |map|
 
-  map.home '', :controller => 'home', :action => 'index'
+  map.root :controller => 'home', :action => 'index'
+  map.connect 'music', :controller => :music, :action => 'index'
+  map.connect 'info', :controller => :info, :action => 'index'
+  map.connect 'pics', :controller => :pics, :action => 'index'
+  map.connect 'shop', :controller => :shop, :action => 'index'
 
-  map.resources :pics, :only => [:index], :collection => {:gallery => :get}
+  map.connect 'damon', :controller => :info, :action => 'damon'
+  map.connect 'david', :controller => :info, :action => 'david'
+  map.connect 'andre', :controller => :info, :action => 'andre'
+  map.connect 'greg', :controller => :info, :action => 'greg'
+  map.connect 'signup', :controller => :info, :action => 'signup'
 
-  map.signingup 'signingup', :controller => :info, :action => 'signingup'
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  map.connect 'ziegfeld', :controller => :music, :action => 'ziegfeld'
+  map.connect 'garden', :controller => :music, :action => 'garden'
+  map.connect 'rebecca', :controller => :music, :action => 'rebecca'
+  map.connect 'these_days', :controller => :music, :action => 'these_days'
+  map.connect 'berlin', :controller => :music, :action => 'berlin'
+
+  map.connect 'signingup', :controller => :info, :action => 'signingup'
 end
