1749128e6ee48348473da99c408e62fdb6ebbc18
David Goudreau <pair+david@pivotallabs.com>
2010-04-14 12:48:09 -0700

    updating galleryView

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -3,7 +3,4 @@ class PicsController < ApplicationController
   def index
   end
 
-  def gallery
-    render :layout => false
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,14 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
-  def currently_selected(selected)
-    selected == @controller.controller_name
-  end
-
-  def gallery_panel(path, title, desc)
+  def gallery_panel(path, desc)
     capture_haml do
-      haml_tag :div, {:class => 'panel'} do
+      haml_tag :li do
         haml_tag :img, {:src => "/images/#{path}"}
         haml_tag :div, { :class => 'panel-overlay' } do
-          haml_tag :h2, title, {:class => 'title'}
+          haml_tag :h2, "&nbsp;", {:class => 'title'}
           haml_tag :p, desc, {:class => 'desc'}
         end
       end
     end
   end
-
-  def gallery_filmstrip(path, alt, title)
-    capture_haml do
-      haml_tag :li do
-        haml_tag :img, {:src => "/images/#{path}", :alt => alt, :title => title}
-      end
-    end
-
-  end
-
 end
