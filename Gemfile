dabdac4f21e1412b75ae6a882598adc500a9f983
David Goudreau <pair+david@pivotallabs.com>
2010-04-20 23:15:24 -0700

    upgrading to bundler 0.9.21

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
-bundle_path "vendor/bundler_gems"
+source :gemcutter
 source "http://gems.github.com"
 source "http://gems.rubyforge.org"
 source "http://gems.engineyard.com"
-source "http://gems.rubyonrails.org"
+#source "http://gems.rubyonrails.org"
 
 gem "rake", "0.8.7"
 gem "rails", "2.3.5"
--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,1 +1,15 @@
-require "#{RAILS_ROOT}/vendor/bundler_gems/environment"
+begin
+  require "rubygems"
+  require "bundler"
+
+  if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.5")
+    raise RuntimeError, "Your bundler version (#{Gem::Version.new(Bundler::VERSION)}) is too old." +
+        "Run `gem install bundler` to upgrade."
+  end
+
+  # Set up load paths for all bundled gems
+  Bundler.setup
+rescue Bundler::GemNotFound
+  raise RuntimeError, "Bundler couldn't find some gems." +
+      "Did you run `bundle install`?"
+end
